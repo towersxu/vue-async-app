f83fa8d4b83150a7fac20ddf89dce41758104d0e
towersxu <987427795@qq.com>
2016-03-09 16:39:41 +0800

    删除无效文件

--- webpack.production.js
+++ webpack.production.js
@@ -3,7 +3,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   entry: {
-    app: './vue/main.js'
+    app: './src/main.js'
   },
   output: {
     path: './build',
@@ -15,16 +15,19 @@ module.exports = {
       { test: /\.vue$/, loader: 'vue' },
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel'},
       { test: /\.(png|jpg)$/, loader: 'file' },
-      { test: /\.(png|jpg)$/, loader: 'url?limit=10000'},
+      //{ test: /\.(png|jpg)$/, loader: 'url?limit=10000'},
       { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
-      { test: /\.less$/, loader: "style!css!less" },
-      { test: /\.css$/, loader: 'style-loader!css-loader' }
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' }
     ]
   },
   plugins: [
+    new webpack.ProvidePlugin({
+      "Vue":"vue"
+    }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
